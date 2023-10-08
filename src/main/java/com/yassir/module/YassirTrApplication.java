0de6d2cb5d86b6c4593ccb231cf4077c30a78100
Yassir Salhi <yassir4salhi@gmail.com>
2023-10-08 17:12:33 +0100

    first commit

---
+++ src/main/java/com/yassir/module/YassirTrApplication.java
@@ -0,0 +1,13 @@
+package com.yassir.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YassirTrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YassirTrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yassir/module/YassirTrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassir.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YassirTrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
